daac76aad5dc159270e6df0c88301e1847c17da0
AidanJay <akj2jackson@gmail.com>
2023-09-14 10:08:30 -0700

    added phone rotation support

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -15,7 +15,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonReset: Button
 
     var isRunning = false
-    var timeWhenStopped = 0L
+    var displayTime = 0L
+    var lapTime = ""
 
     // declaring a classwide constant in java:
     // public static final double PI = 3.14
@@ -25,6 +26,11 @@ class MainActivity : AppCompatActivity() {
         val TAG = "MainActivity"
         // just for github testing purposes
         val PHYSICISTS_PI = 3
+
+        //make constants for your key-value pairs
+        val STATE_DISPLAY_TIME = "the display time"
+        val STATE_LAP_TIME = "the lap time"
+        val STATE_IS_RUNNING = "if the chronometer is running"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,29 +39,51 @@ class MainActivity : AppCompatActivity() {
 
         wireWidgets()
 
+        // restore saveInstanceState if it exists
+        if (savedInstanceState != null) {
+            displayTime = savedInstanceState.getLong(STATE_DISPLAY_TIME)
+            isRunning = savedInstanceState.getBoolean(STATE_IS_RUNNING)
+            lapTime = savedInstanceState.getString(STATE_LAP_TIME).toString()
+            if(!isRunning) {
+                stopwatch.base = displayTime + SystemClock.elapsedRealtime()
+                buttonStartStop.text = "Start"
+                buttonReset.text = "Reset"
+                recentLap.text = lapTime
+                stopwatch.stop()
+            } else {
+                stopwatch.base = displayTime - SystemClock.elapsedRealtime()
+                buttonStartStop.text = "Stop"
+                buttonReset.text = "Lap"
+                recentLap.text = lapTime
+                stopwatch.start()
+            }
+        }
+
         buttonStartStop.setOnClickListener {
             if(!isRunning) {
                 isRunning = true
                 buttonStartStop.text = "Stop"
                 buttonReset.text = "Lap"
-                stopwatch.setBase(SystemClock.elapsedRealtime() + timeWhenStopped)
+                stopwatch.base = SystemClock.elapsedRealtime() + displayTime
                 stopwatch.start()
             } else {
                 isRunning = false
                 buttonStartStop.text = "Start"
                 buttonReset.text = "Reset"
-                timeWhenStopped = stopwatch.getBase() - SystemClock.elapsedRealtime()
+                displayTime = stopwatch.base - SystemClock.elapsedRealtime()
                 stopwatch.stop()
             }
         }
 
         buttonReset.setOnClickListener {
             if(!isRunning) {
-                stopwatch.setBase(SystemClock.elapsedRealtime())
-                timeWhenStopped = 0L
+                stopwatch.base = SystemClock.elapsedRealtime()
+                recentLap.base = SystemClock.elapsedRealtime()
+                displayTime = 0L
                 stopwatch.stop()
             } else {
-                recentLap.setBase(stopwatch.getBase())
+                lapTime = stopwatch.text.toString()
+                recentLap.text = lapTime
             }
         }
     }
@@ -91,6 +119,26 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy: this is a log")
+    }
+
+    // Use this to preserve state through orientation changes
+    override fun onSaveInstanceState(outState: Bundle) {
+        // figure out the time that is currently displayed on the screen
+        // and save that in a key-value pair in the bundle
+        if(isRunning) {
+            displayTime = SystemClock.elapsedRealtime() + stopwatch.base
+            lapTime = recentLap.text.toString()
+            //solve for base:
+            // base = elapsedTime - displayTime
+        }
+        // if it weren't running, you would have saved the displayTime in
+        // the stop button's onClickListener
+
+        // save key-value pairs to the bundle before the superclass call
+        outState.putLong(STATE_DISPLAY_TIME, displayTime)
+        outState.putBoolean(STATE_IS_RUNNING, isRunning)
+        outState.putString(STATE_LAP_TIME, lapTime)
+        super.onSaveInstanceState(outState)
     }
 
     private fun wireWidgets() {
